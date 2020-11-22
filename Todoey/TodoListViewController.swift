297d7dba3dd5cf0d97aff5f427335f6717026532
m-zaink <sidsmith9@gmail.com>
2020-11-22 14:01:15 +0530

    Add checkmarking feature to cells in TodoListVC

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -30,5 +30,19 @@ class TodoListViewController: UITableViewController {
         
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(
+            at: indexPath,
+            animated: true
+        )
+        
+    }
 }
 
